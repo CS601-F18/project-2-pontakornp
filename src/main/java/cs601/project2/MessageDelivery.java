70c31ff4439432cb933c41b8301653ed0d86faac
Pontakorn Pakavaleetorn <ppontakorn@gmail.com>
2018-09-26 00:36:43 -0700

    create main class

---
+++ src/main/java/cs601/project2/MessageDelivery.java
@@ -0,0 +1,10 @@
+package cs601.project2;
+
+public class MessageDelivery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first commit");
+	}
+
+}
